f3be47cd5850ef89f1ac1e206630ccb9b8d13d12
catrionameriel <catriona.meriel@gmail.com>
2017-11-06 16:30:47 +0000

    test_student_favourite_language passed

--- spec/student_class_spec.rb
+++ spec/student_class_spec.rb
@@ -25,4 +25,12 @@ class TestStudent < MiniTest::Test
     @student.cohort_number = 18
     assert_equal(18, @student.cohort_number)
   end
+
+  def test_what_student_says
+   assert_equal("I can talk", @student.say_something)
+  end
+
+  def test_student_favourite_language
+    assert_equal("My favourite language is Ruby", @student.favourite_language("Ruby"))
+  end
 end
--- student_class.rb
+++ student_class.rb
@@ -8,8 +8,11 @@ class Student
     @cohort_number = input_cohort_number
   end
 
-  def name
-    return @name
+  def say_something
+    return "I can talk"
   end
 
+  def favourite_language(favourite_language)
+    return "My favourite language is #{favourite_language}"
+  end
 end
